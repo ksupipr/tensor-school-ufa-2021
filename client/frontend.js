763cc7dc344bd21d37ca3d1bb9f1d732b2cdf371
valeriailikbaeva <valeria180501@mail.ru>
2021-11-09 01:32:05 +0500

    add yaml and node modules

---
+++ client/frontend.js
@@ -0,0 +1,13 @@
+import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.esm.browser.js'
+
+new Vue({
+    el:'#app',
+    data(){
+        return {
+            form:{
+                name: '',
+                value: ''
+            }
+        }
+    }
+})
