78cba6ed90b2ded5a8d6d93b8e29e1b36888b578
Gilyazetdinov Ruslan <kavabangaga@gmail.com>
2021-11-15 23:46:25 +0500

    some changes

--- js/scripts.js
+++ js/scripts.js
@@ -5,26 +5,26 @@ document.addEventListener('DOMContentLoaded', () => {
 
     let modal1 = document.querySelector('#modal-1');
 
-    let closeButton = modal1.querySelector('.modal__close-button');
+    let closeButton = modal1.querySelector('#modal__close-button');
 
     let tagBody = document.querySelector('body');
 
-    callBackButton.onclick = function (e) {
-      e.preventDefault();
+    callBackButton.onclick = function (event) {
+      event.preventDefault();
       modal1.classList.add('modal_active');
       tagBody.classList.add('hidden');
     }
     
-    closeButton.onclick = function (e) {
-      e.preventDefault();
+    closeButton.onclick = function (event) {
+      event.preventDefault();
       modal1.classList.remove('modal_active');
       tagBody.classList.remove('hidden');
     }
   
-    modal1.onmousedown = function (e) {
-      let target = e.target;
+    modal1.onmousedown = function (event) {
+      let target = event.target;
       let modalContent = modal1.querySelector('.modal__content');
-      if (e.target.closest('.' + modalContent.className) === null) {
+      if (event.target.closest('.' + modalContent.className) === null) {
         this.classList.remove('modal_active');
         tagBody.classList.remove('hidden');
       }
